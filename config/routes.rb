9e20212a898a6fd10b16a7da738f53e4126d6944
Francis Poitras <s21385@hotmail.com>
2018-05-15 14:35:33 -0400

    ex 1 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get '/tasks', to: 'tasks#index', as: 'tasks'
+
+get '/new',  to: 'tasks#new', as: 'new_task'
+post '/tasks', to: 'tasks#create',  as: 'create_task'
+
+get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+
+get "/tasks/:id/edit", to: "tasks#edit", as: 'edit_task'
+patch "/tasks/:id", to: "tasks#update"
+
+delete "/tasks/:id", to: "tasks#destroy"
+
+root 'tasks#index'
+
+
 end
